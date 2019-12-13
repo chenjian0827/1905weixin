243718f8d1fcb057010a46b630c9b1095d1def81
cj <cj@qq.com>
2019-12-13 01:56:00 +0000

    debug

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -20,11 +20,18 @@ class WxController extends Controller
         $echostr = $_GET["echostr"];
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-        if( $tmpStr == $signature ){        //验证通过
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if ($tmpStr == $signature) {        //验证通过
             echo $echostr;
-        }else{
+        } else {
             die("not ok");
         }
     }
+
+
+
+
+
+    
+}
