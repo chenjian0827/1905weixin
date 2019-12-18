08e4b3f9ce9dde29bb6f7fb8258ae25e8220f32b
cj <cj@qq.com>
2019-12-18 11:47:58 +0000

    111

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sd', function () {
-    echo 123;
+Route::get('/info', function () {
+    phpinfo();
 });
+
+Route::get('/test/hello','Test\TestController@hello');
+
+Route::any('user/index','User\\LoginController@index');
+Route::any('user/addUser','User\\LoginController@addUser');
+Route::any('user/redis1','User\\LoginController@redis1');
+Route::any('user/redis2','User\\LoginController@redis2');
+Route::any('user/baidu','User\\LoginController@baidu');
+
+
 
 //微信开发
-Route::get('/wx','Wx\WxController@wechat');
-Route::post('/wx','Wx\WxController@aaa');
-Route::get('/aa',function (){
-    echo 123;
-});
+//Route::get('/wx','WeiXin\WxController@wx');
+//Route::post('/wx','WeiXin\WxController@receiv');
 
+
+
+//微信开发
+Route::get('/wx','WeiXin\WxController@wx');
+Route::post('/wx','WeiXin\WxController@receiv');         //接受微信推送事件
+Route::get('/wx/media','WeiXin\WxController@getMedia');  //获取临时素材
+Route::get('/wx/test','WeiXin\WxController@test');       //获取临时素材
+Route::get('/wx/menu','WeiXin\WxController@createMenu'); //创建菜单
+
+Route::post('/wx','WeiXin\WxController@receiv');        //接收微信的推送事件
+Route::get('/wx/media','WeiXin\WxController@getMedia');        //获取临时素材
+Route::get('/wx/flush/access_token','WeiXin\WxController@flushAccessToken');        //刷新access_token
+Route::get('/wx/menu','WeiXin\WxController@createMenu');        //创建菜单
+//微信公众号
+Route::get('/vote','VoteController@index');        //微信投票
